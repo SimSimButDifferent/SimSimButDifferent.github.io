a35ad6aef88006df969af96ab58e6a324f72acfb
simsim <simeon.campbell@live.com>
2023-07-06 16:24:22 +0700

    center content + global css updated

--- my-portfolio/src/pages/index.js
+++ my-portfolio/src/pages/index.js
@@ -10,18 +10,27 @@ export default function Home() {
         <h1 className="text-4xl font-bold p-10">
           {"Simeon Campbell - CAD Design"}
         </h1>
+        <p></p>
       </div>
-      <Image
-        className="rounded-full"
-        src="/images/profile.jpg"
-        height={400}
-        width={400}
-        alt="Picture of the author"
-      />
-      <p>
-        {"I'm a software engineer and a student at the University of Waterloo."}
-      </p>
-      <p>{"I'm currently working on"}</p>
+      <div className="flex items-center justify-center">
+        <Image
+          className="rounded-full"
+          src="/images/profile.jpg"
+          height={400}
+          width={400}
+          alt="Picture of the author"
+        />
+      </div>
+      <div className="text flex items-center justify-center py-40 max-w-3xl text-justify mx-auto text-center">
+        <p>
+          {
+            "I started my design career as an trainee/ intern at Microworkshops Ltd for over 3 years. Initially, helping with day to day tasks, including some SEO for the company website, whilst learning Solidworks and the design process, from my tutor, mentor and friend Elliot Colley. After a few months I started taking the more basic iterations for single part design, before learning the workings of more complex assemblies. After 2 years I was independently working with my own basket of clients, before moving on to taking care of the larger clients. After 3 and a half great years at Microworkshops, I have since been working on my own projects as a freelancer. I now mostly specialise in garden furniture design, pairing that with my plastic part design expertise, I create 3D models that are parametric, easy to modify and make."
+          }
+        </p>
+      </div>
+      <div className="text flex items-center justify-center">
+        <p>{"I'm currently working on"}</p>
+      </div>
     </main>
   );
 }
