1ec397aace3da3c776992aba18487b3da6b0e990
simsim <simeon.campbell@live.com>
2023-07-07 16:21:09 +0700

    h2 fadeinout

--- my-portfolio/src/pages/index.js
+++ my-portfolio/src/pages/index.js
@@ -6,11 +6,19 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <div className="flex items-center justify-center h-screen bg-[./public/images/bambrella-umbrella.jpg]">
-        <h1 className="text-4xl font-bold p-10">
-          {"Simeon Campbell - CAD Design"}
-        </h1>
-        <p></p>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="headings animate-fade-in fade-in">Cad Services</h1>
+        <div className="flex flex-row">
+          <h2 className="subheading pl-2 animate-fadeInOut">
+            Industrial design
+          </h2>
+          <h2 className="subheading pl-2 animate-fadeInOut delay-2000">
+            Rapid Prototyping
+          </h2>
+          <h2 className="subheading pl-2 animate-fadeInOut delay-4000">
+            Design for manufacturing
+          </h2>
+        </div>
       </div>
       <div className="flex items-center justify-center">
         <Image
@@ -23,13 +31,22 @@ export default function Home() {
       </div>
       <div className="text flex items-center justify-center py-40 max-w-3xl text-justify mx-auto text-center">
         <p>
-          {
-            "I started my design career as an trainee/ intern at Microworkshops Ltd for over 3 years. Initially, helping with day to day tasks, including some SEO for the company website, whilst learning Solidworks and the design process, from my tutor, mentor and friend Elliot Colley. After a few months I started taking the more basic iterations for single part design, before learning the workings of more complex assemblies. After 2 years I was independently working with my own basket of clients, before moving on to taking care of the larger clients. After 3 and a half great years at Microworkshops, I have since been working on my own projects as a freelancer. I now mostly specialise in garden furniture design, pairing that with my plastic part design expertise, I create 3D models that are parametric, easy to modify and make."
-          }
+          I started my design career as an trainee/ intern at Microworkshops Ltd
+          for over 3 years. Initially, helping with day to day tasks, including
+          some SEO for the company website, whilst learning Solidworks and the
+          design process, from my tutor, mentor and friend Elliot Colley. After
+          a few months I started taking the more basic iterations for single
+          part design, before learning the workings of more complex assemblies.
+          After 2 years I was independently working with my own basket of
+          clients, before moving on to taking care of the larger clients. After
+          3 and a half great years at Microworkshops, I have since been working
+          on my own projects as a freelancer. I now mostly specialise in garden
+          furniture design, pairing that with my plastic part design expertise,
+          I create 3D models that are parametric, easy to modify and make."
         </p>
       </div>
       <div className="text flex items-center justify-center">
-        <p>{"I'm currently working on"}</p>
+        <p>I'm currently working on</p>
       </div>
     </main>
   );
--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        fadeInOut: "fadeInOut 6s ease-in-out infinite",
+      },
+      keyframes: {
+        fadeInOut: {
+          "0%": { opacity: "0" },
+          "50%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      transitionDelay: { 2000: "2000ms" },
+      plugins: [],
     },
   },
-  plugins: [],
-}
+};
