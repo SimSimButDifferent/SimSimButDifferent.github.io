6a8dfc45da78a877feec25671fb36678b5255870
simsim <simeon.campbell@live.com>
2023-08-07 18:38:47 +0700

    GTM added to _doc.js

--- my-portfolio/src/pages/_document.js
+++ my-portfolio/src/pages/_document.js
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FTY7N2QF09"
+        ></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FTY7N2QF09');
+          `,
+          }}
+        ></Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
