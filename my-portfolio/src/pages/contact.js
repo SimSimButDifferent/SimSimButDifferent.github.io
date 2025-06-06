ff751d2e6e0ea608cb4ff89b440989b8384302af
simsim <simeon.campbell@live.com>
2025-06-06 13:59:09 +0700

    map fix 2

--- my-portfolio/src/pages/contact.js
+++ my-portfolio/src/pages/contact.js
@@ -33,87 +33,89 @@ const ContactPage = () => {
             priority
           />
         </div>
-        <div className="flex flex-col bg-white p-10 md:flex-row pb-10 md:shadow-xl justify-around gap-6 md:gap-10 md:items-stretch">
-          <motion.div
-            className="flex-shrink-0 w-full md:w-auto md:max-w-md h-auto md:h-auto"
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6, delay: 0.2 }}
-            whileHover={{ scale: 1.02 }}
-          >
-            <div className="bg-sky-300 p-4 rounded-2xl shadow-xl h-full">
-              <div className="flex flex-col bg-white rounded-2xl shadow-xl text-center p-4 h-full justify-center">
-                <motion.h1
-                  className="flex headings justify-center"
-                  initial={{ opacity: 0, y: -30 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.6 }}
-                >
-                  Contact
-                </motion.h1>
-                <div className="textboxes">
-                  <p className="pb-5 text-center lg:text-left">
-                    If you would like to discuss a project of yours
-                  </p>
-                  <p className="pb-5 text-center lg:text-left">
-                    Get in touch today!
-                  </p>
-                  <div className="flex flex-row justify-center md:justify-start">
-                    <a href="tel:+442476981341">
-                      <Image
-                        src="/images/call_sky_400.svg"
-                        height={30}
-                        width={30}
-                        alt="Call icon of a phone from google icons"
-                      ></Image>
-                    </a>
-                    <a
-                      href="tel:+442476981341"
-                      className="text-sky-400 hover:text-sky-600 pl-3"
+        <div className="bg-white p-10 pb-10 md:shadow-xl">
+          <div className="max-w-7xl mx-auto">
+            <div className="flex flex-col md:flex-row gap-6 md:gap-10 md:items-stretch">
+              <motion.div
+                className="w-full md:w-1/2 h-auto md:h-auto pl-0 md:pl-[10px]"
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6, delay: 0.2 }}
+                whileHover={{ scale: 1.02 }}
+              >
+                <div className="bg-sky-300 p-4 rounded-2xl shadow-xl h-full">
+                  <div className="flex flex-col bg-white rounded-2xl shadow-xl text-center p-4 h-full justify-center">
+                    <motion.h1
+                      className="flex headings justify-center"
+                      initial={{ opacity: 0, y: -30 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{ duration: 0.6 }}
                     >
-                      +44 (0)24 7698 1341
-                    </a>
-                  </div>
-                  <p className="py-5 text-center lg:text-left">
-                    Or just drop us a message at
-                  </p>
-                  <div className="flex flex-row justify-center md:justify-start">
-                    <a
-                      href="mailto:dreaminrealitycad@gmail.com"
-                      className="pr-3"
-                    >
-                      <Image
-                        src="/images/mail_sky_400.svg"
-                        height={30}
-                        width={30}
-                        alt="Call icon of a phone from google icons"
-                      ></Image>
-                    </a>
-                    <div className="flex flex-col text-sky-400 hover:text-sky-600">
-                      <Link href="mailto:dreaminrealitycad@gmail.com">
-                        dreaminrealitycad
-                      </Link>
-                      <Link
-                        href="mailto:dreaminrealitycad@gmail.com"
-                        className="pl-6"
-                      >
-                        @gmail.com
-                      </Link>
+                      Contact
+                    </motion.h1>
+                    <div className="textboxes">
+                      <p className="pb-5 text-center">
+                        If you would like to discuss a project of yours
+                      </p>
+                      <p className="pb-5 text-center">Get in touch today!</p>
+                      <div className="flex flex-row justify-center">
+                        <a href="tel:+442476981341">
+                          <Image
+                            src="/images/call_sky_400.svg"
+                            height={30}
+                            width={30}
+                            alt="Call icon of a phone from google icons"
+                          ></Image>
+                        </a>
+                        <a
+                          href="tel:+442476981341"
+                          className="text-sky-400 hover:text-sky-600 pl-3"
+                        >
+                          +44 (0)24 7698 1341
+                        </a>
+                      </div>
+                      <p className="py-5 text-center">
+                        Or just drop us a message at
+                      </p>
+                      <div className="flex flex-row justify-center">
+                        <a
+                          href="mailto:dreaminrealitycad@gmail.com"
+                          className="pr-3"
+                        >
+                          <Image
+                            src="/images/mail_sky_400.svg"
+                            height={30}
+                            width={30}
+                            alt="Call icon of a phone from google icons"
+                          ></Image>
+                        </a>
+                        <div className="flex flex-col text-sky-400 hover:text-sky-600">
+                          <Link href="mailto:dreaminrealitycad@gmail.com">
+                            dreaminrealitycad
+                          </Link>
+                          <Link
+                            href="mailto:dreaminrealitycad@gmail.com"
+                            className="pl-6"
+                          >
+                            @gmail.com
+                          </Link>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </motion.div>
+              </motion.div>
 
-          <motion.div
-            className="flex-1 h-auto md:h-auto"
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6, delay: 0.4 }}
-          >
-            <GoogleMap address="Dream InReality, Swan Lane, Sellindge, Ashford, UK" />
-          </motion.div>
+              <motion.div
+                className="w-full md:w-1/2 h-auto md:h-auto pr-0 md:pr-[10px]"
+                initial={{ opacity: 0, x: 50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6, delay: 0.4 }}
+              >
+                <GoogleMap address="Dream InReality, Swan Lane, Sellindge, Ashford, UK" />
+              </motion.div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
