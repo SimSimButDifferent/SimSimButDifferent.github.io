df3963671a162ac8172a16f9bbe8eca0704f60c7
simsim <simeon.campbell@live.com>
2025-06-08 12:58:05 +0700

    contact button fixed - header

--- my-portfolio/src/pages/index.js
+++ my-portfolio/src/pages/index.js
@@ -101,11 +101,11 @@ export default function Home() {
                 for mass-production
               </motion.p>
               <motion.div
-                className="pt-3 xl:pt-5"
+                className="pt-3 xl:pt-5 w-fit"
                 initial={{ opacity: 0, scale: 0.8 }}
                 animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5, delay: 0.9 }}
-                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+                whileHover={{ scale: 1.15 }}
                 whileTap={{ scale: 0.95 }}
               >
                 <Link
